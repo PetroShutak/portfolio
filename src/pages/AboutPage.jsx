11f71704d2ff602e5985a9d6ccbb7a58a8de3a60
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-11-20 08:47:50 +0200

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,8 +9,12 @@ const AboutPage = () => {
       </Helmet>
       <div style={{
         height: '100vh',
+        paddingTop: '60px',
+        paddingBottom: '60px',
       }}>
-<h2>
+<h2 style={{
+        color: 'white',
+      }}>
 I am a passionate developer with a keen interest in building web applications. My skills range from front-end to back-end development, and I enjoy working on projects that challenge me to learn and grow.
 </h2>
 </div>
